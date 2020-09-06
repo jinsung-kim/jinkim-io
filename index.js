fc7c6ec50e56f01f9dac08080242ad33fcf172e6
Jin Kim <jinsungkim1210@gmail.com>
2020-09-05 20:40:36 -0700

    feat: first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000; // Can also set to 443 or 80
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html'); // Used to load the site
+});
+
+// Used to open resume
+app.get('/resume', (req, res) => {
+    res.sendFile(__dirname + '/resume.pdf');
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
