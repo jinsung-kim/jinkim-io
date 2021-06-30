3487a26f6099b9805013b1f0da7d026d125b3166
Jin Kim <jinsungkim1210@gmail.com>
2021-06-30 11:10:45 -0700

    feat: about page created

--- index.js
+++ index.js
@@ -17,15 +17,22 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html'); // Used to load the site
 });
 
+// Used to redirect to the about page
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + "/about.html");
+});
+
 // Used to open resume
 app.get('/resume', (req, res) => {
     res.sendFile(__dirname + '/resume.pdf');
 });
 
+// Used to redirect to the pictures page
 app.get('/pictures', (req, res) => {
     res.sendFile(__dirname + '/pictures.html');
 });
 
+// Used to redirect to the Jekyll blog when it's up
 app.get("/blog", (req, res) => {
     res.redirect("https://jinsung-kim.github.io/");
 });
